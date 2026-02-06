8c83c73656378022cdd52165e9d8856e87891475
Dmitry Olegovich Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2026-02-06 16:51:11 +0300

    Initial Commit ...

---
+++ merchant/lib/services/server.dart
@@ -0,0 +1,186 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:foodbank_marchantise_app/services/api-list.dart';
+
+class Server {
+  /*static final Server _instance = Server._internal();
+  factory Server() => _instance;
+  Server._internal();*/
+
+  static String? bearerToken;
+
+  static initClass({String? token}) {
+    bearerToken = token!;
+  }
+
+  getRequest({String? endPoint}) async {
+    HttpClient client = HttpClient();
+
+    try {
+      client.badCertificateCallback =
+          ((X509Certificate cert, String host, int port) => true);
+      return await http.get(Uri.parse(APIList.server! + endPoint!),
+          headers: _getHttpHeaders());
+    } catch (error) {
+      return null;
+    } finally {
+      client.close();
+    }
+  }
+
+  getRequestSettings(endPoint) async {
+    HttpClient client = HttpClient();
+    try {
+      return await http.get(Uri.parse(APIList.server! + endPoint!),
+          headers: getAuthHeaders());
+    } catch (error) {
+      return null;
+    } finally {
+      client.close();
+    }
+  }
+
+  getRequestWithParam({String? endPoint, var orderId}) async {
+    HttpClient client = HttpClient();
+    try {
+      client.badCertificateCallback =
+          ((X509Certificate cert, String host, int port) => true);
+      return await http.get(
+          Uri.parse(APIList.server! + "restaurant-order/$orderId"),
+          headers: _getHttpHeaders());
+    } catch (error) {
+      return null;
+    } finally {
+      client.close();
+    }
+  }
+
+  multipartRequest({String? endPoint, body, String? filepath, type}) async {
+    Map<String, String> headers = {
+      'Authorization': bearerToken!,
+      'Content-Type': 'multipart/form-data',
+    };
+    HttpClient client = HttpClient();
+    try {
+      var request;
+      if (type) {
+        request = http.MultipartRequest(
+            'POST', Uri.parse(APIList.server! + endPoint!))
+          ..fields.addAll(body)
+          ..headers.addAll(headers)
+          ..files.add(await http.MultipartFile.fromPath('image', filepath!));
+      } else {
+        request = http.MultipartRequest(
+            'POST', Uri.parse(APIList.server! + endPoint!))
+          ..fields.addAll(body)
+          ..headers.addAll(headers);
+      }
+      return await request.send();
+    } catch (error) {
+      return null;
+    } finally {
+      client.close();
+    }
+  }
+
+  postRequest({String? endPoint, String? body}) async {
+    HttpClient client = HttpClient();
+    try {
+      client.badCertificateCallback =
+          ((X509Certificate cert, String host, int port) => true);
+      return await http.post(Uri.parse(APIList.server! + endPoint!),
+          headers: getAuthHeaders(), body: body);
+    } catch (error) {
+      //  return null;
+    } finally {
+      client.close();
+    }
+  }
+
+  postRequestWithToken({String? endPoint, String? body}) async {
+    HttpClient client = HttpClient();
+    try {
+      // client.badCertificateCallback = ((X509Certificate cert, String host, int port) => true);
+      return await http.post(Uri.parse(APIList.server! + endPoint!),
+          headers: _getHttpHeaders(), body: body);
+    } catch (error) {
+      return null;
+    } finally {
+      client.close();
+    }
+  }
+
+  putRequest({String? endPoint, String? body}) async {
+    HttpClient client = HttpClient();
+    try {
+      client.badCertificateCallback =
+          ((X509Certificate cert, String host, int port) => true);
+      return await http.put(Uri.parse(APIList.server! + endPoint!),
+          headers: _getHttpHeaders(), body: body);
+    } catch (error) {
+      return null;
+    } finally {
+      client.close();
+    }
+  }
+
+  putRequestChangeStatus({String? endPoint, String? body, int? id}) async {
+    HttpClient client = HttpClient();
+    try {
+      client.badCertificateCallback =
+          ((X509Certificate cert, String host, int port) => true);
+      return await http.put(Uri.parse(APIList.server! + endPoint!),
+          headers: _getHttpHeaders(), body: body);
+    } catch (error) {
+      return null;
+    } finally {
+      client.close();
+    }
+  }
+
+  deleteRequest({String? endPoint}) async {
+    HttpClient client = HttpClient();
+    try {
+      client.badCertificateCallback =
+          ((X509Certificate cert, String host, int port) => true);
+      return await http.delete(Uri.parse(APIList.server! + endPoint!),
+          headers: _getHttpHeaders());
+    } catch (error) {
+      return null;
+    } finally {
+      client.close();
+    }
+  }
+
+  static Map<String, String> _getHttpHeaders() {
+    Map<String, String> headers = new Map<String, String>();
+
+    headers['Authorization'] = bearerToken!;
+    headers['content-type'] = 'application/json';
+    return headers;
+  }
+
+  static Map<String, String> getAuthHeaders() {
+    Map<String, String> headers = new Map();
+    headers['content-type'] = 'application/json';
+    return headers;
+  }
+
+  customGetRequest({String? endPoint, String? token}) async {
+    HttpClient client = HttpClient();
+    try {
+      // client.badCertificateCallback = ((X509Certificate cert, String host, int port) => true);
+      return await http.get(
+        Uri.parse(APIList.server! + endPoint!),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': token!
+        },
+      );
+    } catch (error) {
+      return null;
+    } finally {
+      client.close();
+    }
+  }
+}
